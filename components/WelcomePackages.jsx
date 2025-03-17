3b710edd22abb9d82df16b267660a64040abbefc
uniquetheo <55855459+uniquetheo@users.noreply.github.com>
2025-03-17 11:25:56 +0000

    Welecome package section responsive optimization

--- components/WelcomePackages.jsx
+++ components/WelcomePackages.jsx
@@ -4,16 +4,20 @@ import { Button } from "./ui/button";
 const WelcomePackages = () => {
   return (
     <div className="w-full py-8 bg-gray-200 ">
-      <div className="flex flex-col items-center justify-center gap-4 px-4 md:px-8 lg:px-12">
-        <h3 className="text-sm uppercase font-bold text-center">
+      <div className="container mx-auto px-4">
+        <h3 className="text-sm uppercase font-bold text-center mb-6">
           🎁 Sign Up & Get:
         </h3>
-        <ul className="">
-          <li>✅ Up to 50% Discount on Membership Fees (Limited-Time Only!)</li>
-          <li>✅ Up to £500 Travel Credits for Hotel Bookings</li>
-        </ul>
-        <div className="">
-          <Button>Join Now & Start Saving!</Button>
+        <div className="flex flex-col w-full border lg:flex-row items-center justify-center lg:justify-evenly gap-4">
+          <ul className="">
+            <li>
+              ✅ Up to 50% Discount on Membership Fees (Limited-Time Only!)
+            </li>
+            <li>✅ Up to £500 Travel Credits for Hotel Bookings</li>
+          </ul>
+          <div className="">
+            <Button>Join Now & Start Saving!</Button>
+          </div>
         </div>
       </div>
     </div>
