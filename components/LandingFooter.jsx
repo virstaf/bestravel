ca5c405012c0b85922fdb53f87100278e5e17245
uniquetheo <unique.theo98@gmail.com>
2025-04-29 14:17:22 +0000

    Reverse icons on footer

--- components/LandingFooter.jsx
+++ components/LandingFooter.jsx
@@ -7,7 +7,7 @@ const LandingFooter = () => {
   return (
     <footer className="w-full">
       <div className="w-full py-8 bg-slate-900 text-(--muted)">
-        <div className="container flex flex-wrap justify-between items-center mx-auto px-4 md:px-8 lg:px-12">
+        <div className="container flex flex-row-reverse flex-wrap justify-between items-center mx-auto px-4 md:px-8 lg:px-12">
           <div className="Socials flex gap-3 my-3">
             {socialLinks.map((item, idx) => (
               <Link key={idx} href={item.url} target="_blank">
