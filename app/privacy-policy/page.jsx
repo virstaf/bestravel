f4476e9f38d372232c69d0e9f1352df6436a2070
uniquetheo <unique.theo98@gmail.com>
2025-05-15 08:30:57 +0000

    create privacy policy page route

---
+++ app/privacy-policy/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PrivacyPage = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      PrivacyPage
+    </div>
+  );
+};
+
+export default PrivacyPage;
