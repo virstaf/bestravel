1e6a5c957e7eb4761deeffaafda76a710fb03a0f
uniquetheo <unique.theo98@gmail.com>
2025-07-08 10:19:12 +0000

    Add in house trial system

--- actions/stripe.js
+++ actions/stripe.js
@@ -1,6 +1,8 @@
 "use server";
 
 import { stripe } from "@/lib/stripe";
+import { createClient } from "@supabase/supabase-js";
+import { sub } from "date-fns";
 
 export const subscribeAction = async (user, priceId) => {
   // console.log("Subscribing user:", user, "with priceId:", priceId);
@@ -24,4 +26,33 @@ export const subscribeAction = async (user, priceId) => {
     cancel_url: `${process.env.NEXT_PUBLIC_BASEURL}`,
   });
   return url;
+};
+
+export const trialAction = async (user) => {
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_KEY
+  );
+
+  const { data, error } = await supabaseAdmin
+    .from("profiles")
+    .update({
+      trial_start: new Date().toISOString(), // Current time in ISO format
+      trial_ends_at: new Date(
+        Date.now() + 7 * 24 * 60 * 60 * 1000
+      ).toISOString(), // 7 days from now
+      is_subscribed: true, // Set to true to indicate trial subscription
+      subscription_status: "trialing", // Set status to trialing
+      subscription_end: new Date(
+        Date.now() + 7 * 24 * 60 * 60 * 1000
+      ).toISOString(), // Set end date to 7 days from now
+    })
+    .eq("id", user.id)
+    .select() // Return the updated record
+    .single();
+
+  if (!error) {
+  }
+
+  return { data, error };
 };
--- components/ui/pricing.jsx
+++ components/ui/pricing.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState, useTransition } from "react";
 import { getUser } from "@/lib/supabase/server";
 import { pricingPlans } from "@/lib/constants";
 import { Button } from "./button";
-import { subscribeAction } from "@/actions/stripe";
+import { subscribeAction, trialAction } from "@/actions/stripe";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
@@ -40,67 +40,107 @@ const Pricing = () => {
     });
   };
 
+  const handleStartTrialClick = async () => {
+    if (!user) {
+      toast.error("You must be logged in to start a trial.");
+      return;
+    }
+    startTransition(async () => {
+      const { data, error } = await trialAction(user);
+      if (error) {
+        toast.error("Trial start failed. Please try again.");
+        console.error("Trial start error:", error);
+        return;
+      }
+      if (data) {
+        router.push("dashboard");
+      } else {
+        toast.error("Trial start failed. Please try again.");
+        console.error("Trial start failed");
+      }
+    });
+  };
+
   return (
-    <div className="max-w-7xl mx-auto px-4 py-8 bg-white rounded-lg shadow-xl">
-      <div className="w-[200px] mx-auto my-8">
-        <div className="duration-toggle bg-gray-200 border border-gray-300 rounded-md p-0.5 ">
-          <Button
-            className="font-medium w-1/2"
-            variant={duration === "monthly" ? "" : "primary"}
-            onClick={() => setDuration("monthly")}
-          >
-            Monthly
-          </Button>
-          <Button
-            className="font-medium w-1/2"
-            variant={duration === "yearly" ? "" : "primary"}
-            onClick={() => setDuration("yearly")}
-          >
-            Yearly
-          </Button>
+    <>
+      <div className="max-w-7xl mx-auto px-4 py-8 bg-white rounded-lg shadow-xl">
+        <div className="w-[200px] mx-auto my-8">
+          <div className="duration-toggle bg-gray-200 border border-gray-300 rounded-md p-0.5 ">
+            <Button
+              className="font-medium w-1/2"
+              variant={duration === "monthly" ? "" : "primary"}
+              onClick={() => setDuration("monthly")}
+            >
+              Monthly
+            </Button>
+            <Button
+              className="font-medium w-1/2"
+              variant={duration === "yearly" ? "" : "primary"}
+              onClick={() => setDuration("yearly")}
+            >
+              Yearly
+            </Button>
+          </div>
+        </div>
+        <div className="max-w-6xl mx-auto flex flex-col md:flex-row gap-3 items-start justify-center py-8 p-4">
+          {pricingPlans.map((plan) => (
+            <div key={plan.id} className=" p-6 w-full max-w-sm">
+              <h2 className="text-xl font-semibold mb-6">{plan.name}</h2>
+              <div className="flex gap-1 items-center h-full mb-6">
+                <p className="text-3xl font-bold">
+                  <span className="font-bold">£</span>
+                  {duration === "monthly" ? plan.price[0] : plan.price[1]}
+                </p>
+                <span className="text-gray-500 font-light leading-4">
+                  per <br />
+                  {duration === "monthly" ? "month" : "year"}
+                </span>
+              </div>
+              <Button
+                className={`my-4 py-5 w-full text-white text-[16px] ${
+                  isPending ? "bg-gray-400 cursor-not-allowed" : ""
+                }`}
+                onClick={() =>
+                  handleSubscribeClick(
+                    duration === "monthly" ? plan.priceId[0] : plan.priceId[1]
+                  )
+                }
+                disabled={isPending}
+              >
+                {isPending ? "Processing..." : "Subscribe"}
+              </Button>
+              <ul className="pl-5 text-[14px] text-gray-600">
+                {plan.features.map((feature) => (
+                  <li
+                    key={feature}
+                    className="py-1.5 flex items-center gap-0.5"
+                  >
+                    <span className="text-white text-[10px] mr-2 bg-gray-400 rounded-full w-1.5 h-1.5 p-2 flex items-center justify-center">
+                      ✓
+                    </span>
+                    {feature}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
-      <div className="max-w-6xl mx-auto flex flex-col md:flex-row gap-3 items-start justify-center py-8 p-4">
-        {pricingPlans.map((plan) => (
-          <div key={plan.id} className=" p-6 w-full max-w-sm">
-            <h2 className="text-xl font-semibold mb-6">{plan.name}</h2>
-            <div className="flex gap-1 items-center h-full mb-6">
-              <p className="text-3xl font-bold">
-                <span className="font-bold">£</span>
-                {duration === "monthly" ? plan.price[0] : plan.price[1]}
-              </p>
-              <span className="text-gray-500 font-light leading-4">
-                per <br />
-                {duration === "monthly" ? "month" : "year"}
-              </span>
-            </div>
+      <section className="w-full container mx-auto pb-8 px-4">
+        <div className="bg-gradient-to-r from-primary to-secondary rounded-xl p-8 text-center text-white my-12">
+          <h2 className="text-3xl font-bold mb-4">Want to try it out?</h2>
+          <div className="text-lg text-primary mb-4">
             <Button
-              className={`my-4 py-5 w-full text-white text-[16px] ${
-                isPending ? "bg-gray-400 cursor-not-allowed" : ""
-              }`}
-              onClick={() =>
-                handleSubscribeClick(
-                  duration === "monthly" ? plan.priceId[0] : plan.priceId[1]
-                )
-              }
-              disabled={isPending}
+              variant="outline"
+              className="cursor-pointer mt-5 hover:scale-105 transition-transform"
+              onClick={handleStartTrialClick}
             >
-              {isPending ? "Processing..." : "Subscribe"}
+              Start Free 7-Day Trial
             </Button>
-            <ul className="pl-5 text-[14px] text-gray-600">
-              {plan.features.map((feature) => (
-                <li key={feature} className="py-1.5 flex items-center gap-0.5">
-                  <span className="text-white text-[10px] mr-2 bg-gray-400 rounded-full w-1.5 h-1.5 p-2 flex items-center justify-center">
-                    ✓
-                  </span>
-                  {feature}
-                </li>
-              ))}
-            </ul>
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      </section>
+    </>
   );
 };
 
