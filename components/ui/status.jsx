70238cfd527ead5238979b48a4ce157f9adcb5f7
uniquetheo <unique.theo98@gmail.com>
2025-12-02 21:59:51 +0000

    fix: cookies error

--- components/ui/status.jsx
+++ components/ui/status.jsx
@@ -4,21 +4,21 @@ const Status = ({ value }) => {
   return (
     <span
       className={`h-8 flex items-center font-mono tracking-wider justify-center px-4 rounded-full text-xs font-medium ${
-        value === "confirmed"
+        value === "confirmed" //when client has agreed to quote
           ? "bg-green-100 text-green-800"
-          : value === "pending"
+          : value === "pending" // when client submits a reservaton
             ? "bg-orange-100 text-orange-800"
-          : value === "planning"
+          : value === "planning" // 
             ? "bg-blue-100 text-blue-800"
-            : value === "rejected"
+            : value === "rejected" //
               ? "bg-red-100 text-red-800"
-              : value === "cancelled"
+              : value === "cancelled" //
                 ? "bg-gray-100 text-gray-800"
-                : value === "in review"
+                : value === "in review" // when admin is working on it and has
                   ? "bg-blue-100 text-blue-800"
-                  : value === "expired"
+                  : value === "expired" // when quote validation
                     ? "bg-yellow-100 text-yellow-800"
-                    : value === "active"
+                    : value === "active" // when client has paid
                       ? "bg-green-100 text-green-800"
                       : "bg-red-100 text-red-800"
       }`}
