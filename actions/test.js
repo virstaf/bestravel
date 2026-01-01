25517e57be410aaf25c1fd2b36443aee1646fb21
uniquetheo <unique.theo98@gmail.com>
2026-01-01 12:46:38 +0000

    troubleshoot: remove server log

--- actions/test.js
+++ actions/test.js
@@ -2,7 +2,7 @@
 
 export const testAction = async (user) => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  console.log(user);
+  //   console.log(user);
 
-  return { success: true };
+  return { success: true, user };
 };
